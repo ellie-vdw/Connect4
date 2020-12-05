b90366d8c8152c232cd2951848504336bb22d3c5
Ellen VandeWater <evandewater515@ku.edu>
2020-12-04 20:23:53 -0600

    project2 updates

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Connect 4
 Author: Ellen Vandewater <br>
 This is for EECS 368 JavaScript Project. I have choosen to create the game of Connect 4 for my project.
+For Project 2 of this class, I have extended this game to be played as connect4 or connect5. Connect5 is when players have to get 5 pieces in a row on the same sized board. Making things more difficult. I also updated some of the CSS to look nicer. 
--- main2.js
+++ main2.js
@@ -2,6 +2,7 @@ var tRow = document.getElementsByTagName('tr');
 var tCell = document.getElementsByTagName('td');
 var tSlot = document.querySelectorAll('.slot');
 const playerTurn = document.querySelector('.pturn');
+const connectNum = document.querySelector('.connect');
 const reset = document.querySelector('.reset');
 
 for (let i=0; i<tCell.length; i++){
@@ -18,55 +19,109 @@ while(!p2){
     var p2= prompt('Player Two: Enter your name, you will be yellow.');
 }
 p2Color='gold';
+while(!fourOrFive){
+    var fourOrFive = prompt('Would you like to play connect4 or connect5? (enter either the number 4 or 5)');
+}
+let cNum = parseInt(fourOrFive);
 let currentPlayer =1;
-playerTurn.textContent= `${p1}'s turn`;
+playerTurn.textContent= `${p1}'s turn!`;
+connectNum.textContent=`Connect${fourOrFive}`;
+console.log(cNum);
 
 Array.prototype.forEach.call(tCell, (cell) =>{
     cell.addEventListener('click', markColor);
     cell.style.backgroundColor ='white';
 });
 
+
+
 function markColor(e){
     let col = e.target.cellIndex;
     let row = [];
-
-    for (let i=5; i>-1; i--){
-        if (tRow[i].children[col].style.backgroundColor == 'white'){
-            row.push(tRow[i].children[col]);
-            if (currentPlayer ===1){
-                row[0].style.backgroundColor = p1Color;
-                if(horizontalCheck()|| verticalCheck()|| diagonalCheckSE()|| diagonalCheckSW()){
-                    playerTurn.textContent = `${p1} wins!`;
-                    return(alert(`${p1} wins!`));
-                }
-                else if (tie())
-                {
-                    playerTurn.textContent = `ITS A TIE`;
-                    return(alert('TIE GAME'));
-                }
-                else{
-                    playerTurn.textContent = `${p2}'s turn!`;
-                    return currentPlayer=2;
-                }
-                
-            }
-            else{
-                row[0].style.backgroundColor = p2Color;
-                if(horizontalCheck()|| verticalCheck()|| diagonalCheckSE()|| diagonalCheckSW()){
-                    playerTurn.textContent = `${p2} wins!`;
-                    return(alert(`${p2} wins!`));
-                }
-                else if (tie())
-                {
-                    playerTurn.textContent = `ITS A TIE`;
-                    return(alert('TIE GAME'));
+//main game loop
+//i=5 reps the number of rows
+    if(cNum == 4){
+        for (let i=5; i>-1; i--){
+            if (tRow[i].children[col].style.backgroundColor == 'white'){
+                row.push(tRow[i].children[col]);
+                if (currentPlayer ===1){
+                    row[0].style.backgroundColor = p1Color; //drops p1 marker
+                    if(horizontalCheck()|| verticalCheck()|| diagonalCheckSE()|| diagonalCheckSW()){
+                        playerTurn.textContent = `${p1} wins!`;
+                        return(alert(`${p1} wins!`));
+                    }
+                    else if (tie())
+                    {
+                        playerTurn.textContent = `ITS A TIE`;
+                        return(alert('TIE GAME'));
+                    }
+                    else{
+                        playerTurn.textContent = `${p2}'s turn!`;
+                        return currentPlayer=2;
+                    }
+                    
                 }
                 else{
-                    playerTurn.textContent = `${p1}'s turn!`;
-                    return currentPlayer=1;
+                    row[0].style.backgroundColor = p2Color; //drops p2 marker
+                    if(horizontalCheck()|| verticalCheck()|| diagonalCheckSE()|| diagonalCheckSW()){
+                        playerTurn.textContent = `${p2} wins!`;
+                        return(alert(`${p2} wins!`));
+                    }
+                    else if (tie())
+                    {
+                        playerTurn.textContent = `ITS A TIE`;
+                        return(alert('TIE GAME'));
+                    }
+                    else{
+                        playerTurn.textContent = `${p1}'s turn!`;
+                        return currentPlayer=1;
+                    }
                 }
             }
         }
+    }
+    else if (cNum == 5){
+        for (let i=5; i>-1; i--){
+            if (tRow[i].children[col].style.backgroundColor == 'white'){
+                row.push(tRow[i].children[col]);
+                if (currentPlayer ===1){
+                    row[0].style.backgroundColor = p1Color; //drops p1 marker
+                    if(horizontalCheckFive()|| verticalCheckFive()|| diagonalCheckSEFive()|| diagonalCheckSWFive()){
+                        playerTurn.textContent = `${p1} wins!`;
+                        return(alert(`${p1} wins!`));
+                    }
+                    else if (tie())
+                    {
+                        playerTurn.textContent = `ITS A TIE`;
+                        return(alert('TIE GAME'));
+                    }
+                    else{
+                        playerTurn.textContent = `${p2}'s turn!`;
+                        return currentPlayer=2;
+                    }
+                    
+                }
+                else{
+                    row[0].style.backgroundColor = p2Color; //drops p2 marker
+                    if(horizontalCheckFive()|| verticalCheckFive()|| diagonalCheckSEFive()|| diagonalCheckSWFive()){
+                        playerTurn.textContent = `${p2} wins!`;
+                        return(alert(`${p2} wins!`));
+                    }
+                    else if (tie())
+                    {
+                        playerTurn.textContent = `ITS A TIE`;
+                        return(alert('TIE GAME'));
+                    }
+                    else{
+                        playerTurn.textContent = `${p1}'s turn!`;
+                        return currentPlayer=1;
+                    }
+                }
+            }
+        }
+    }
+    else{
+        alert("Invalid choice for game play. Please refresh and enter either the NUMBER 4 or 5");
     }
 }
 
@@ -75,11 +130,26 @@ function checkColor (a,b,c,d){
     return (a == b && a==c && a==d && a!=='white');
 }
 
+function checkColorFive(a,b,c,d,e){
+    return (a==b && a==c && a==d && a==e && a!=='white');
+}
+
 function horizontalCheck(){
-    for (let r=0; r<tRow.length; r++){
-        for(let c=0; c<4; c++){
+    for (let r=0; r<tRow.length; r++){ //checks all the rows so 6
+        for(let c=0; c<4; c++){ //can only win four different ways so only starts at first 4 coulmns
             if(checkColor(tRow[r].children[c].style.backgroundColor, tRow[r].children[c+1].style.backgroundColor, 
                 tRow[r].children[c+2].style.backgroundColor, tRow[r].children[c+3].style.backgroundColor)){
+                    return true;
+                }
+        }
+    }
+}
+
+function horizontalCheckFive(){
+    for (let r=0; r<tRow.length; r++){ //checks all rows
+        for (let c=0; c<3; c++){ //can only win three ways so only starts a the frist three columns
+            if(checkColorFive(tRow[r].children[c].style.backgroundColor, tRow[r].children[c+1].style.backgroundColor, 
+                tRow[r].children[c+2].style.backgroundColor, tRow[r].children[c+3].style.backgroundColor, tRow[r].children[c+4].style.backgroundColor)){
                     return true;
                 }
         }
@@ -97,6 +167,18 @@ function verticalCheck(){
     }
 }
 
+function verticalCheckFive(){
+    for(let c=0; c<7; c++){
+        for (let r= 0; r<2; r++){
+            if(checkColorFive(tRow[r].children[c].style.backgroundColor, tRow[r+1].children[c].style.backgroundColor,
+                tRow[r+2].children[c].style.backgroundColor, tRow[r+3].children[c].style.backgroundColor, tRow[r+4].children[c].style.backgroundColor)){
+                    return true;
+                }
+        }
+
+    }
+}
+
 function diagonalCheckSE(){
     for (let c=0; c<4; c++){
         for(let r=0; r<3; r++){
@@ -108,11 +190,34 @@ function diagonalCheckSE(){
     }
 }
 
+function diagonalCheckSEFive(){
+    for (let c=0; c<3; c++){
+        for (let r=0; r<2; r++){
+            //console.log(c, r, "\n");
+            if(checkColorFive(tRow[r].children[c].style.backgroundColor, tRow[r+1].children[c+1].style.backgroundColor,
+                tRow[r+2].children[c+2].style.backgroundColor,tRow[r+3].children[c+3].style.backgroundColor, tRow[r+4].children[c+4].style.backgroundColor)){
+                    return true;
+            }
+        }
+    }
+}
+
 function diagonalCheckSW(){
     for (let c=0; c<4; c++){
         for(let r=5; r>2; r--){
             if(checkColor(tRow[r].children[c].style.backgroundColor, tRow[r-1].children[c+1].style.backgroundColor,
                 tRow[r-2].children[c+2].style.backgroundColor,tRow[r-3].children[c+3].style.backgroundColor)){
+                    return true;
+                }
+        }
+    }
+}
+
+function diagonalCheckSWFive(){
+    for (let c=0; c<3; c++){
+        for(let r=5; r>3; r--){
+            if(checkColorFive(tRow[r].children[c].style.backgroundColor, tRow[r-1].children[c+1].style.backgroundColor,
+                tRow[r-2].children[c+2].style.backgroundColor,tRow[r-3].children[c+3].style.backgroundColor, tRow[r-4].children[c+4].style.backgroundColor)){
                     return true;
                 }
         }
@@ -136,6 +241,6 @@ reset.addEventListener('click', ()=>{
     tSlot.forEach(slot =>{
         slot.style.backgroundColor = 'white';
     });
-    playerTurn.style.color = 'black';
-    return (currentPlayer === 1 ? playerTurn.textContent = `${p1}'s turn`: playerTurn.textContent = `${p2}'s turn`);
+    //playerTurn.style.color = 'black';
+    return (currentPlayer === 1 ? playerTurn.textContent = `${p1}'s turn!`: playerTurn.textContent = `${p2}'s turn!`);
 })
